c0fbfb95647f3d5780d7b797b67b614b24994272
gphillips12 <98001052+gphillips12@users.noreply.github.com>
2022-01-22 21:35:45 -0700

    Add files via upload
    Project 1

---
+++ PhillipsP1.py
@@ -0,0 +1,14 @@
+#PhillipsP1
+#Programmer: Garrett Phillips
+#EMail: gphillips12@cnm.edu
+#Purpose: Provides user ability to calculate volume of a pyramid
+#Python Version: 2.5
+
+
+#Length of the base: a Height of the pyramid: h Volume = a2h / 3 Slant height,
+# s = sqrt(h2 + (a / 2) 2) This is the Pythagorean Theorem part.
+# Area of one pyramid side = s * a / 2
+
+print "Hi, I'm the pyramid calculator. I am here to "
+
+h = input ("What is the height of your pyramid?")
